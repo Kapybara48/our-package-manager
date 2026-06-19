1b4d5758b12df7c9c0725a1c2b63ebaf3ed5005d
Kapybara48 <daniel.krejci44@gmail.com>
2026-06-19 23:39:59 +0200

    git Clone()

---
+++ git.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"strconv"
+)
+
+func Clone(url string, destination string, depth int, branch string) error {
+	cmd := exec.Command("git", "clone", "--depth", strconv.Itoa(depth), "--branch", branch, url, destination)
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("error cloning git repository %s", err)
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+)
 
 func main() {
-fmt.Println("Hello world")
-} 
+	//	nFlag := flag.Int("n", 1234, "help message for flag n")
+	//	flag.Parse()
+	//	if *nFlag == 1234 {
+	//		fmt.Println("n flag used")
+	//	}
+
+	//gitUrlFlag := flag.String("git-url", "", "link for git project")
+	flag.Parse()
+	err := Clone("https://github.com/Foxboron/sbctl.git", "/tmp/sbctl-git", 1, "master")
+	if err != nil {
+		panic(err)
+	}
+}
