ce1e562deacb9780b451039584630842ff7470e3
bekucera <bekucera@gmail.com>
2026-06-21 22:54:12 +0200

    package helper and config helper created, some example code in main.go

---
+++ package-helper/package-helper.go
@@ -0,0 +1,26 @@
+package packagehelper
+
+import (
+	"log"
+
+	confighelper "our-package-manager/config-helper"
+	githelper "our-package-manager/git-helper"
+	makehelper "our-package-manager/make-helper"
+)
+
+func Install(packageConfig *confighelper.PackageConfig) error {
+	git := githelper.GitRepository{URL: packageConfig.Url, Depth: 1}
+	err := git.Clone()
+	if err != nil {
+		panic(err)
+	}
+
+	err = makehelper.MakeTarget(git.Directory, packageConfig.Makefile, "install")
+	if err != nil {
+		return err
+	}
+
+	log.Println("\"%s\" installed successfully", git.URL)
+
+	return nil
+}
