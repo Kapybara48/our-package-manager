7822a7ecfd85f06c9bcbc753f34e55da9dc7febd
bekucera <bekucera@gmail.com>
2026-06-21 23:13:27 +0200

    git clone branch added

--- config-helper/config-helper.go
+++ config-helper/config-helper.go
@@ -3,6 +3,7 @@ package confighelper
 type PackageConfig struct {
 	Url            string
 	GitCloneDepth  string
+	GitCloneBranch string
 	Makefile       string
 	MakefileTarget string
 	InstallScript  string
