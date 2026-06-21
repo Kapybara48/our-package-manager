7de56fcc78410a44a294f3ec773836f35be8660a
Kapybara48 <daniel.krejci44@gmail.com>
2026-06-21 23:31:11 +0200

    random folder name of git repo

--- git-helper/git-helper.go
+++ git-helper/git-helper.go
@@ -3,15 +3,30 @@ package githelper
 
 import (
 	"fmt"
+	"math/rand"
 	"os/exec"
 	"strconv"
 	"strings"
+
+	confighelper "our-package-manager/config-helper"
 )
 
 type GitRepository struct {
-	URL    string
-	Depth  int
-	Branch string
+	URL       string
+	Depth     int
+	Branch    string
+	Directory string
+}
+
+func NewGitRepository(packageConfig confighelper.PackageConfig) *GitRepository {
+	git := GitRepository{}
+
+	git.URL = packageConfig.URL
+	git.Depth = packageConfig.GitCloneDepth
+	git.Branch = packageConfig.GitCloneBranch
+	git.Directory = generateFolderName(git.URL)
+
+	return &git
 }
 
 func (g *GitRepository) Clone() error {
@@ -38,8 +53,17 @@ func (g *GitRepository) Clone() error {
 func generateFolderName(url string) string {
 	parts := strings.Split(url, "/")
 	repoName := strings.TrimSuffix(parts[len(parts)-1], ".git")
-	return "/tpm/" + repoName
+	return "/tpm/" + repoName + generateRandomString(10)
 }
 
 func generateRandomString(length int) string {
+	charset := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+	charsetLength := len(charset)
+	randomChars := make([]byte, length)
+
+	for i := range length {
+		randomChars[i] = charset[rand.Intn(charsetLength)]
+	}
+
+	return string(randomChars)
 }
