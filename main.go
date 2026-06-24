28617b4891c204552135697c63e5a39100c83647
bekucera <bekucera@gmail.com>
2026-06-24 23:35:52 +0200

    updating test

--- main.go
+++ main.go
@@ -2,26 +2,36 @@ package main
 
 import (
 	"flag"
+	"os"
+	"path/filepath"
 
+	confighelper "our-package-manager/config-helper"
 	packagehelper "our-package-manager/package-helper"
 )
 
 func main() {
 	installFlag := flag.String("install", "", "--install git url of package you want to install")
-	selfUpdateFlag := flag.Bool("self-update", false, "Self updates our-package-manager")
+	updateFlag := flag.Bool("update", false, "Updates all installed packages")
 
 	flag.Parse()
 
-	if *selfUpdateFlag {
-		packageConfig, err := packagehelper.GetPackageConfig("https://github.com/Kapybara48/our-package-manager.git")
-		if err != nil {
-			panic(err)
-		}
-		err = packagehelper.Install(packageConfig)
+	if *updateFlag {
+		packageConfigFiles, err := os.ReadDir("/etc/our/packages/")
 		if err != nil {
 			panic(err)
 		}
 
+		for _, p := range packageConfigFiles {
+
+			packageConfig, err := confighelper.ReadPackageConfig(filepath.Join("/etc/our/packages/", p.Name()))
+			if err != nil {
+				panic(err)
+			}
+			err = packagehelper.Install(packageConfig)
+			if err != nil {
+				panic(err)
+			}
+		}
 		return
 	}
 
