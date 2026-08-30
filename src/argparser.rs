ccfc68ca951bc59e600670c4a3caf750d0243c19
Kapybara48 <daniel.krejci44@gmail.com>
2026-08-30 11:31:19 +0200

    added clap for args

--- src/argparser.rs
+++
@@ -1,9 +0,0 @@
-use std::env;
-
-pub fn print_args() {
-    let args = env::args().skip(1);
-
-    for arg in args {
-        println!("{}", arg);
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
-mod argparser;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    name: String,
+}
 
 fn main() {
-    argparser::print_args();
+    let args = Args::parse();
+
+    println!("{}", args.name);
 }
