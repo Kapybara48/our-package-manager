7a4e92aa474a1a9fd27324615aa0abf472fc909c
Kapybara48 <daniel.krejci44@gmail.com>
2026-09-04 15:08:57 +0200

    subcommands added

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,46 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
+#[derive(Parser)]
+#[command()]
 struct Args {
-    #[arg(short, long)]
+    #[arg(short)]
     name: String,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Install,
+    Remove,
+    Update,
 }
 
 fn main() {
     let args = Args::parse();
 
     println!("{}", args.name);
+}
+
+fn main_copy() {
+    let mut args = std::env::args();
+    match args.nth(1) {
+        Some(arg) => subcommand(arg.as_str(), &mut args),
+        None => println!("no option selected/help menu"),
+    }
+}
+fn subcommand(arg: &str, args: &mut std::env::Args) {
+    match arg {
+        "install" => install(args),
+        "remove" => println!("removing..."),
+        "update" => println!("updating..."),
+        _ => println!("unknown command"),
+    };
+}
+fn install(args: &mut std::env::Args) {
+    match args.next() {
+        Some(arg) => println!("Installing {}", arg),
+        None => println!("no package selected"),
+    }
 }
