79064353a27a97d8ad0b82eb8c67726f7601fad2
Kapybara48 <daniel.krejci44@gmail.com>
2026-09-04 23:33:04 +0200

    set clone destination

--- src/git.rs
+++ src/git.rs
@@ -1,11 +1,21 @@
 use std::process::{Command, ExitStatus};
 
 pub fn clone(url: &str) -> Result<ExitStatus, std::io::Error> {
-    match Command::new("git").args(["clone", url]).spawn() {
+    match Command::new("git")
+        .args(["clone", url, get_name_from_url(url)])
+        .spawn()
+    {
         Ok(mut child) => match child.wait() {
             Ok(exit_status) => Ok(exit_status),
             Err(error) => Err(error),
         },
         Err(error) => Err(error),
     }
+}
+
+fn get_name_from_url(url: &str) -> &str {
+    url.trim_end_matches(".git")
+        .rsplit("/")
+        .next()
+        .unwrap_or("package")
 }
