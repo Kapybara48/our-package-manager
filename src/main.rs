24b1b2128e4e56ce2b9459dcc6fd44ce5288a06d
Kapybara48 <daniel.krejci44@gmail.com>
2026-09-04 15:32:18 +0200

    making install fn

--- src/main.rs
+++ src/main.rs
@@ -18,30 +18,12 @@ fn main() {
     let args = Args::parse();
 
     match args.command {
-        Commands::Install { url } => println!("installing {}", url),
+        Commands::Install { url } => install(&url),
         Commands::Remove => println!("removing"),
         Commands::Update => println!("updating"),
     }
 }
 
-fn main_copy() {
-    let mut args = std::env::args();
-    match args.nth(1) {
-        Some(arg) => subcommand(arg.as_str(), &mut args),
-        None => println!("no option selected/help menu"),
-    }
-}
-fn subcommand(arg: &str, args: &mut std::env::Args) {
-    match arg {
-        "install" => install(args),
-        "remove" => println!("removing..."),
-        "update" => println!("updating..."),
-        _ => println!("unknown command"),
-    };
-}
-fn install(args: &mut std::env::Args) {
-    match args.next() {
-        Some(arg) => println!("Installing {}", arg),
-        None => println!("no package selected"),
-    }
+fn install(url: &String) {
+    println!("installing {}", url);
 }
