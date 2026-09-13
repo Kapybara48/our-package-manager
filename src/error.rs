cc7194569b7a53653c15cb98236eac76342ed948
Kapybara48 <daniel.krejci44@gmail.com>
2026-09-13 12:53:22 +0200

    feat: error type for toml parsing

--- src/error.rs
+++ src/error.rs
@@ -3,6 +3,7 @@ use std::fmt::Formatter;
 pub enum OurError {
     MissingHomeEnv,
     Io(std::io::Error),
+    TomlParse(toml::de::Error),
 }
 
 impl From<std::io::Error> for OurError {
@@ -11,11 +12,18 @@ impl From<std::io::Error> for OurError {
     }
 }
 
+impl From<toml::de::Error> for OurError {
+    fn from(error: toml::de::Error) -> Self {
+        OurError::TomlParse(error)
+    }
+}
+
 impl std::fmt::Display for OurError {
     fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
         match self {
             OurError::MissingHomeEnv => write!(f, "Could not find your home directory"),
             OurError::Io(error) => write!(f, "I/O error: {}", error),
+            OurError::TomlParse(error) => write!(f, "Error while parsing toml: {}", error),
         }
     }
 }
