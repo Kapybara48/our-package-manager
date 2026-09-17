72aeab2bc1bf83e19a33352de3c626e8fc14cbec
Kapybara48 <daniel.krejci44@gmail.com>
2026-09-17 22:54:26 +0200

    feat: paths - packages, temp

--- rust/src/paths.rs
+++ rust/src/paths.rs
@@ -20,3 +20,19 @@ pub fn get_bin_dir() -> Result<PathBuf, OurError> {
 
     Ok(our_bin_dir)
 }
+
+pub fn get_packages_dir() -> Result<PathBuf, OurError> {
+    let mut our_package_dir = get_our_dir()?;
+    our_package_dir.push("packages");
+    create_dir_all(&our_package_dir)?;
+
+    Ok(our_package_dir)
+}
+
+pub fn get_temp_dir() -> Result<PathBuf, OurError> {
+    let mut our_temp_dir = get_our_dir()?;
+    our_temp_dir.push("temp");
+    create_dir_all(&our_temp_dir)?;
+
+    Ok(our_temp_dir)
+}
